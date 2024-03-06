9eb7add8450bedfaa73682d534a1e20665355978
TasnadiRichard <tasnadiricsi88@gmail.com>
2024-03-06 16:22:27 +0100

    kesz

--- Konyvadatkereso/Program.cs
+++ Konyvadatkereso/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Konyvadatkereso;
 
 namespace Konyvadatkereso
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
+            Console.WriteLine("Kérem adja meg az könyv ISBN számát: ");
+            string isbn = Console.ReadLine();
+            await book(isbn);
+            Console.ReadLine();
+        }
+
+        private static async Task book(string json)
+        {
+            string apiUrl = $"https://www.goodreads.com/book/isbn/isbn?key=apiKey";
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(jsonResponse);
+                }
+                else
+                {
+                    Console.WriteLine("Valami hiba lépett fel a köny keresése során.");
+                }
+            }
         }
     }
 }
